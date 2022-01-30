d3932dcbce80df80bdd5807683a44877510dd6bb
alejandro111555 <al.mirskoy@gmail.com>
2022-01-30 21:04:09 +0200

    Create 2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+// create html
+const items = galleryItems
+  .map(({ preview, original, description }) => {
+    const listEl = `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    return listEl;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", items);
+
+galleryRef.addEventListener("click", onImageClick);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+function onImageClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+}
